274d134424605bce60bbd40c056ea599c8705c6e
Haadi Shakir <hmshakir2@gmail.com>
2025-02-10 16:28:30 -0600

    deleted unnecessary code and spaces

--- main.js
+++ main.js
@@ -19,11 +19,6 @@ function getFetch(){
       .then(data => {
         console.log(data)
 
-        // if(data.media_type === 'image'){
-        //     document.querySelector('img').src = data.hdurl
-        // }else if(data.media_type === 'video'){
-        //     document.querySelector('iframe').src = data.url
-        // }
         
         document.querySelector('h2').innerText = data.event[0].strEvent
         document.querySelector('h3').innerText = data.event[0].strFilename
